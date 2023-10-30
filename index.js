1f4901babe22b9488b0936bd0f7bd432c713e0f7
Anne Hastings <anne.miriam@gmail.com>
2023-10-29 19:46:27 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0,2);
+};
+const returnLastTwoDrivers = function() {
+    return drivers.slice(2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function(value){
+        return value * fare;
+    }
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversArr, func) {
+    return func(driversArr);
+};
